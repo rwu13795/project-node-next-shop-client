183f17805109996af389c33178a0666d2c9ed9da
raywu <rwu13795@gmail.com>
2022-01-04 16:16:06 -0500

    bought the hooby dyno in heroku to use the https for the server

--- pages/shop/men/index.tsx
+++ pages/shop/men/index.tsx
@@ -29,7 +29,7 @@ const MenMainCatPage: NextPage<MainCat_PageProps> = ({
   const client = browserClient();
   const click = async () => {
     const { data } = await client.get(
-      "https://node-next-shop-server.herokuapp.com/api/auth/user-status"
+      "https://node-next-shop-server.xyz/api/auth/user-status"
     );
     console.log(data);
   };
