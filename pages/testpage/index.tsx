db9227c91638d3d00309bd1880a9d0208d0d6b72
raywu <rwu13795@gmail.com>
2022-02-11 03:16:26 -0500

    signed cookies are working now, last test

--- pages/testpage/index.tsx
+++ pages/testpage/index.tsx
@@ -8,7 +8,9 @@ const TestPage: NextPage = ({}) => {
 
   const [signedUrl, setSignedUrl] = useState<string>("");
 
-  const loadCookie = async () => {
+  const [urls, setUrls] = useState({ a: "", b: "", c: "" });
+
+  const loadCookie = useCallback(async () => {
     try {
       const { data } = await client.get(
         `${process.env.NEXT_PUBLIC_SERVER_URL}/testing-cloud-front`
@@ -19,27 +21,30 @@ const TestPage: NextPage = ({}) => {
     } catch (err: any) {
       console.log(err);
     }
-  };
+  }, [client]);
+
+  useEffect(() => {
+    loadCookie();
+    setTimeout(() => {
+      console.log("showing images");
+      setUrls({
+        a: `https://cdn.node-next-shop-rw.store/testing/cat1.jpg`,
+        b: `https://cdn.node-next-shop-rw.store/testing/cat2.jpg`,
+        c: `https://cdn.node-next-shop-rw.store/testing/cat3.jpg`,
+      });
+    }, 3000);
+  }, [loadCookie]);
 
   return (
     <main>
       <div>
         <h1>Testing image</h1>
-        <img
-          src={`https://cdn.node-next-shop-rw.store/testing/cat1.jpg`}
-          alt="1"
-        />
-        <img
-          src={`https://cdn.node-next-shop-rw.store/testing/cat2.jpg`}
-          alt="1"
-        />
-        <img
-          src={`https://cdn.node-next-shop-rw.store/testing/cat3.jpg`}
-          alt="1"
-        />
+        <img src={urls.a} alt="1" />
+        <img src={urls.b} alt="1" />
+        <img src={urls.c} alt="1" />
         <img src={`https://cdn.node-next-shop-rw.store/cat1.jpg`} alt="1" />
         <img src={signedUrl} alt="signedUrl" />
-        <button onClick={loadCookie}>load cookie</button>
+        {/* <button onClick={loadCookie}>load cookie</button> */}
       </div>
     </main>
   );
