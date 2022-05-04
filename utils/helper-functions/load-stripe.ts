ec15a3c1c6dc164dd8e4b4664eac21a455a5f1c9
raywu <rwu13795@gmail.com>
2022-05-04 02:58:20 -0400

    update

--- utils/helper-functions/load-stripe.ts
+++ utils/helper-functions/load-stripe.ts
@@ -1,5 +1,3 @@
 import { loadStripe } from "@stripe/stripe-js";
 
-export const stripePromise = loadStripe(
-  `pk_test_51JGCljAR9sxjwFqat4nydBMdw2u7pxgK6U1N7IyeN0DPTbxFLma5AQsjYYHFtfTbzvNCJEwU4LzSapkjOTH62a1400yJWEgNCS`
-);
+export const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PK!);
